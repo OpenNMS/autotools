85e89783d3c092ab1967bd88b9b0d7bd0034c074
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-07-25 14:47:46 +0000

    put autogen in the autotools bit

---
+++ autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+[ -z "$ACLOCAL"    ] && ACLOCAL=aclocal
+[ -z "$AUTOHEADER" ] && AUTOHEADER=autoheader
+[ -z "$LIBTOOLIZE" ] && LIBTOOLIZE=glibtoolize
+[ -z "$AUTOCONF"   ] && AUTOCONF=autoconf
+[ -z "$AUTOMAKE"   ] && AUTOMAKE=automake
+
+[ -x "`which libtoolize 2>/dev/null`" ] && LIBTOOLIZE=libtoolize
+
+$ACLOCAL -I m4
+$LIBTOOLIZE --automake --copy --force
+$AUTOHEADER --force
+$AUTOCONF --force
+$AUTOMAKE --add-missing --copy
